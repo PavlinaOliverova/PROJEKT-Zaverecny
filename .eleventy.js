4e67b0b8141d7465b44eb4a7e2ec0dbe4567c56e
Pavlína Oliverová <pavlina.oliver@gmail.com>
2023-05-23 13:32:24 +0200

    favicon

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
 	// Zkopírovat css/ to _site/css/
 	eleventyConfig.addPassthroughCopy("css");
 
-  eleventyConfig.addPassthroughCopy("favicon-16x16.png");
+  eleventyConfig.addPassthroughCopy({"favicon":"/"});
 
   return {
     // možné formáty šablon
